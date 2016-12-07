af43f2f1aed9e6633a4707336ce6bd1ef1d1194e
ASSEENONSCREEN\pablo.rodriguez <pablo.rodriguez@asos.com>
2016-12-07 15:56:29 +0000

    returns false when given one as a year

---
+++ LeapYearKata/LeapYearKata.Tests/LeapYearShould.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LeapYearKata.Tests
+{
+    [TestFixture]
+    public class LeapYearShould
+    {
+        [TestCase]
+        public void ReturnFalse_WhenVerifyingLeapYears_Given1AsLeapYear()
+        {
+            var leapYearVerifier = new LeapYear();
+
+            var isLeapYear = leapYearVerifier.IsLeapYear(1);
+
+            isLeapYear.Should().BeFalse();
+        }
+    }
+
+    public class LeapYear
+    {
+        public bool IsLeapYear(int year)
+        {
+            return false;
+        }
+    }
+}
